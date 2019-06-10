c1b5f72f5ca8ff8a2d09493cb7805e12bccb0bfc
Doug Wright <mrdougwright@gmail.com>
2019-06-09 20:58:46 -0700

    bubble sort

---
+++ practice/sorting-algorithms/bubble-sort.py
@@ -0,0 +1,56 @@
+# Bubble Sort
+# In place sorting
+# O(n^2)
+# fmt: off
+wakeup_times = [
+  16, 49, 3, 12, 56, 49, 55, 22, 13,
+  46, 19, 55, 46, 13, 25, 56, 9, 48, 45
+]
+# fmt: on
+
+
+def bubble_sort_1(l):
+    for iteration in range(len(l)):
+        for index in range(1, len(l)):
+            this = l[index]
+            prev = l[index - 1]
+
+            if prev <= this:
+                continue
+
+            l[index] = prev
+            l[index - 1] = this
+
+
+bubble_sort_1(wakeup_times)
+print("Pass" if (wakeup_times[0] == 3) else "Fail")
+
+
+# Entries are (h, m) where h is the hour and m is the minute
+sleep_times = [(24, 13), (21, 55), (23, 20), (22, 5), (24, 23), (21, 58), (24, 3)]
+
+
+def bubble_sort_2(l):
+    for iteration in range(len(l)):
+        for index in range(1, len(l)):
+            this_hour, this_min = l[index]
+            prev_hour, prev_min = l[index - 1]
+
+            if prev_hour > this_hour or (
+                prev_hour == this_hour and prev_min > this_min
+            ):
+                continue
+
+            l[index] = (prev_hour, prev_min)
+            l[index - 1] = (this_hour, this_min)
+
+
+bubble_sort_2(sleep_times)
+print(
+    "Pass"
+    if (
+        sleep_times
+        == [(24, 23), (24, 13), (24, 3), (23, 20), (22, 5), (21, 58), (21, 55)]
+    )
+    else "Fail"
+)
