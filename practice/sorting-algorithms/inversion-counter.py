f9090fbb03ae12b0848f5a3a6f2387ebfce63089
Doug Wright <mrdougwright@gmail.com>
2019-06-11 20:16:13 -0700

    merge sort

---
+++ practice/sorting-algorithms/inversion-counter.py
@@ -0,0 +1,67 @@
+# The number of *inversions* in a disordered list is the number of pairs of elements that are inverted (out of order) in the list.
+
+# Given an array `arr[0 ... n-1]` of `n` distinct positive integers, for indices `i and j`, if `i < j` and `arr[i] > arr[j]` then the pair `(i, j)` is called an inversion of `arr`.
+def count_inversions(arr):
+    start_index = 0
+    end_index = len(arr) - 1
+    return inversion_count(arr, start_index, end_index)
+
+
+def inversion_count(arr, start_index, end_index):
+    if start_index >= end_index:
+        return 0
+
+    mid_index = start_index + (end_index - start_index) // 2
+
+    left = inversion_count(arr, start_index, mid_index)
+    right = inversion_count(arr, mid_index + 1, end_index)
+
+    output = left + right
+
+    output += merge_two_sorted_halves(
+        arr, start_index, mid_index, mid_index + 1, end_index
+    )
+    return output
+
+
+def merge_two_sorted_halves(arr, start_one, end_one, start_two, end_two):
+    count = 0
+    left_index = start_one
+    right_index = start_two
+
+    output_length = (end_two - start_two + 1) + (end_one - start_one + 1)
+    output_list = [0 for _ in range(output_length)]
+    index = 0
+
+    while index < output_length:
+        # if left <= right, it's not an inversion
+        if arr[left_index] <= arr[right_index]:
+            output_list[index] = arr[left_index]
+            left_index += 1
+
+        else:
+            count = count + (
+                end_one - left_index + 1
+            )  # left > right hence it's an inversion
+            output_list[index] = arr[right_index]
+            right_index += 1
+
+        index = index + 1
+
+        if left_index > end_one:
+            for i in range(right_index, end_two + 1):
+                output_list[index] = arr[i]
+                index += 1
+            break
+
+        elif right_index > end_two:
+            for i in range(left_index, end_one + 1):
+                output_list[index] = arr[i]
+                index += 1
+            break
+
+    index = start_one
+    for i in range(output_length):
+        arr[index] = output_list[i]
+        index += 1
+    return count
---
+++ practice/sorting-algorithms/merge-sort.py
@@ -0,0 +1,41 @@
+# Merge Sort
+# multiply number of iterations by number of comparisons
+# O(n log(n) )
+# space complexity is more than bubble sort (new arrays)
+def mergesort(items):
+    # Base case, a list of 0 or 1 items is already sorted
+    if len(items) <= 1:
+        return items
+
+    # Otherwise, find the midpoint and split the list
+    mid_index = len(items) // 2
+    left = items[:mid_index]
+    right = items[mid_index:]
+
+    # Call mergesort recursively with the left and right half
+    left = mergesort(left)
+    right = mergesort(right)
+
+    # Merge our two halves and return
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+    return merged, count
+
+
+mergesort([3, 1, 2, 9, 8, 7])
