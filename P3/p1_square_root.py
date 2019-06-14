c1269c30663edfa5c989c3f08b9811ca154a3b39
Doug Wright <mrdougwright@gmail.com>
2019-06-14 12:38:47 -0700

    one way to get square root

---
+++ P3/p1_square_root.py
@@ -0,0 +1,29 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    if number == 0 or number == 1:
+        return number
+
+    nums = list(range(1, number))
+    previous = None
+
+    for num in nums:
+        square_root = num * num
+        if square_root > number:
+            return previous
+        elif square_root == number:
+            return num
+        previous = num
+
+
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
