e20bf8c2194ab0615053200a18b7b04078a8c909
Doug Wright <mrdougwright@gmail.com>
2019-06-16 16:47:59 -0700

    review feedback 1

--- P3/p3_rearrange_elems.py
+++ P3/p3_rearrange_elems.py
@@ -7,6 +7,12 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
+    if input_list is None:
+        return None
+
+    if len(input_list) <= 1:
+        return input_list
+
     items = reverse_mergesort(input_list)
     list1 = list()
     list2 = list()
@@ -65,5 +71,30 @@ test_function([[1, 2, 3, 4, 5], [542, 31]])
 
 print(rearrange_digits([4, 6, 2, 5, 9, 8]))  # [964, 852]]
 print(rearrange_digits([2, 1]))  # [2, 1]
+print(rearrange_digits([42]))  # [42]
+print(rearrange_digits(None))  # None
 print(rearrange_digits([2, 1, 9, 7, 8]))  # [971, 82]
 print(rearrange_digits([8, 7, 6, 4, 2, 1]))  # [862, 741]
+
+
+def rearrange_digits_udacity_solution(input_list):
+    num1 = 0
+    num2 = 0
+
+    digit_place = 1  # ones, tens, hundreds etc. places
+    flag = 1  # 1 for num1, 2 for num2
+
+    for digit in range(10):
+        for num in input_list:
+            if num == digit:
+                if flag == 1:
+                    num1 += digit * digit_place
+                    flag = 2
+                else:
+                    num2 += digit * digit_place
+                    flag = 1
+
+                    # change digit place (like, tens to hundreds)
+                    digit_place = 10 * digit_place
+
+    return [num1, num2]
