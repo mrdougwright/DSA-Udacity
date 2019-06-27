465b45624890a992947e9332f3143b0db3c74ed2
Doug Wright <mrdougwright@gmail.com>
2019-06-27 12:02:51 -0700

    add route to determine best path

--- P4/route_planner.py
+++ P4/route_planner.py
@@ -5,6 +5,8 @@ from helpers import Map, load_map, show_map
 
 def shortest_path(M, start, goal):
     visited = []
+    route = []
+
     heap = [(0, (start, start))]
 
     while len(heap) > 0:
@@ -13,10 +15,11 @@ def shortest_path(M, start, goal):
         if destination in visited:
             continue
 
+        route.append((prev_dest, destination))
         visited.append(destination)
 
         if destination == goal:
-            return visited
+            return print_route(route)
 
         prev_path_cost = distance(
             M.intersections[prev_dest], M.intersections[destination]
@@ -27,7 +30,8 @@ def shortest_path(M, start, goal):
             )
             estimated = distance(M.intersections[neighbor], M.intersections[goal])
             heapq.heappush(heap, (path_cost + estimated, (destination, neighbor)))
-    return visited
+
+    return print_route(route)
 
 
 def distance(start, end):
@@ -35,9 +39,20 @@ def distance(start, end):
     return math.hypot(end[0] - start[0], end[1] - start[1])
 
 
+def print_route(route):
+    prev, next = route.pop(0)
+    best_path = [next]
+    while len(route) > 0:
+        prev, next = route.pop(0)
+        if best_path[-1] == prev:
+            best_path.append(next)
+    print(best_path)
+    return best_path
+
+
 # works:
 map_40 = load_map("map-40.pickle")
 shortest_path(map_40, 5, 34)  # path: [5, 16, 37, 12, 34]
-# doesnt:
+# works!:
 map_10 = load_map("map-10.pickle")
-shortest_path(map_10, 2, 0)  # path: [2, 3, 0]
+shortest_path(map_10, 2, 0)  # path: [2, 3, 5, 0]
