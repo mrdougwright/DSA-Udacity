1c3fac217818d0fabe817fc5377068626117707c
Doug Wright <mrdougwright@gmail.com>
2019-06-26 20:50:24 -0700

    P4 update v1

--- P4/route_planner.py
+++ P4/route_planner.py
@@ -4,25 +4,30 @@ from helpers import Map, load_map, show_map
 
 
 def shortest_path(M, start, goal):
-    print("shortest path called")
-    graph = create_graph(M.intersections, M.roads, goal)
-    heap = [(0, start)]
-    shortest_path = []
+    visited = []
+    heap = [(0, (start, start))]
 
     while len(heap) > 0:
-        cost, vertex = min(heap)
-        shortest_path.append(vertex)
+        cost, (prev_dest, destination) = heapq.heappop(heap)
 
-        if vertex == goal:
-            return shortest_path
+        if destination in visited:
+            continue
 
-        heap = []
+        visited.append(destination)
 
-        for edge_cost, neighbor in graph[vertex]:
+        if destination == goal:
+            return visited
+
+        prev_path_cost = distance(
+            M.intersections[prev_dest], M.intersections[destination]
+        )
+        for neighbor in M.roads[destination]:
+            path_cost = prev_path_cost + distance(
+                M.intersections[destination], M.intersections[neighbor]
+            )
             estimated = distance(M.intersections[neighbor], M.intersections[goal])
-            heap.append((edge_cost + estimated, neighbor))
-
-    return shortest_path
+            heapq.heappush(heap, (path_cost + estimated, (destination, neighbor)))
+    return visited
 
 
 def distance(start, end):
@@ -30,20 +35,9 @@ def distance(start, end):
     return math.hypot(end[0] - start[0], end[1] - start[1])
 
 
-def create_graph(intersections, road_list, goal):
-    adjacency_list = [list() for _ in range(len(road_list) + 1)]
-
-    for i, intersection_list in enumerate(road_list):
-        for node in intersection_list:
-            path_cost = distance(intersections[i], intersections[node])
-            adjacency_list[i].append((path_cost, node))
-
-    return adjacency_list
-
-
 # works:
-# map_40 = load_map("map-40.pickle")
-# shortest_path(map_40, 5, 34)  # path: [5, 16, 37, 12, 34]
+map_40 = load_map("map-40.pickle")
+shortest_path(map_40, 5, 34)  # path: [5, 16, 37, 12, 34]
 # doesnt:
 map_10 = load_map("map-10.pickle")
-shortest_path(map_10, 2, 0)  # path: [2, 3, 5, 0]
+shortest_path(map_10, 2, 0)  # path: [2, 3, 0]
